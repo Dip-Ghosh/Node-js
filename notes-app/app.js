e83e45e750db56521250875b132e5fa16f07584e
Dip Ghosh <dipnsucse@gmail.com>
2026-02-09 22:22:52 +0600

    add file to another file to use that

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,10 @@
-const fileSystem = require('fs');
+// const fileSystem = require('fs');
+//
+// fileSystem.writeFileSync('notes.txt', 'This file is written to a note without appending. ');
+// fileSystem.appendFileSync('notes.txt', '\nThis file is written to a note with  appending');
 
-fileSystem.writeFileSync('notes.txt', 'This file is written to a note without appending. ');
-fileSystem.appendFileSync('notes.txt', '\nThis file is written to a note with  appending');
+const add = require('./utils.js');
+
+const sum = add(4, -2);
+
+console.log(sum);
---
+++ notes-app/utils.js
@@ -0,0 +1,10 @@
+console.log('utlis.js');
+
+const firstName = 'Dip';
+
+const add =function (a, b) {
+    return a + b;
+}
+
+module.exports = firstName;
+module.exports = add;
