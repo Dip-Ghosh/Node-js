586842fe2a0fdebcaa44282aa3ee2970c9273ced
Dip Ghosh <dipnsucse@gmail.com>
2026-02-09 22:10:57 +0600

    challenge: appending file with sync done

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fileSystem = require('fs');
 
-fileSystem.writeFileSync('notes.txt', 'This file is written to a note.');
+fileSystem.writeFileSync('notes.txt', 'This file is written to a note without appending. ');
+fileSystem.appendFileSync('notes.txt', '\nThis file is written to a note with  appending');
