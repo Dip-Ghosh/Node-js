efd39f80271b8aa085a663becb93ad15e93b78da
Dip Ghosh <dipnsucse@gmail.com>
2026-02-09 22:28:49 +0600

    challen: new file and return node from function

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,8 +3,10 @@
 // fileSystem.writeFileSync('notes.txt', 'This file is written to a note without appending. ');
 // fileSystem.appendFileSync('notes.txt', '\nThis file is written to a note with  appending');
 
-const add = require('./utils.js');
+// const add = require('./utils.js');
+const note = require('./note.js');
+// const sum = add(4, -2);
+// console.log(sum);
 
-const sum = add(4, -2);
-
-console.log(sum);
+const writtenNote = note('This is the note.')
+console.log(writtenNote);
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,10 +1,8 @@
 console.log('utlis.js');
 
-const firstName = 'Dip';
-
-const add =function (a, b) {
+const add =function(a, b) {
     return a + b;
 }
 
-module.exports = firstName;
+// module.exports = firstName;
 module.exports = add;
