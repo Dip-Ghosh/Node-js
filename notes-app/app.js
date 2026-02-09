f52870ed278a46708b642202c35d28addcf5fc78
Dip Ghosh <dipnsucse@gmail.com>
2026-02-09 22:54:28 +0600

    note validator pacakge install and practice

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,5 @@
-// const fileSystem = require('fs');
-//
-// fileSystem.writeFileSync('notes.txt', 'This file is written to a note without appending. ');
-// fileSystem.appendFileSync('notes.txt', '\nThis file is written to a note with  appending');
 
-// const add = require('./utils.js');
-const note = require('./note.js');
-// const sum = add(4, -2);
-// console.log(sum);
+const validator = require('validator');
+console.log(validator.isEmail('gmail.com'));
+console.log(validator.isURL('https:/gmail.com'));
 
-const writtenNote = note('This is the note.')
-console.log(writtenNote);
