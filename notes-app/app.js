b8a16ca62d5f6ffefc002823b08be1da9acd0fd4
Dip Ghosh <dipnsucse@gmail.com>
2026-02-09 23:17:30 +0600

    remove node modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
 
 const validator = require('validator');
+const chalk = require('chalk');
 console.log(validator.isEmail('gmail.com'));
 console.log(validator.isURL('https:/gmail.com'));
+console.log("this is "+ chalk.blue.inverse.bold('Hello world Dip!') + "");
+
 
