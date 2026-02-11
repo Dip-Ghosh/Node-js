580582aad7414a85ff55f2ded7f138c9a6156186
Dip Ghosh <dipnsucse@gmail.com>
2026-02-11 20:47:11 +0600

    input from argumenet

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 
 const validator = require('validator');
 const chalk = require('chalk');
-console.log(validator.isEmail('gmail.com'));
-console.log(validator.isURL('https:/gmail.com'));
-console.log("this is "+ chalk.blue.inverse.bold('Hello world Dip!') + "");
 
+const command = process.argv[2];
 
+if (command === 'add') {
+    console.log('Adding new note!');
+} else if (command === 'remove') {
+    console.log('Remove new note!');
+}
