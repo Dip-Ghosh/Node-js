e25fde8252d4d8376652c9463c43123f2ee86728
Dip Ghosh <dipnsucse@gmail.com>
2026-02-15 20:52:28 +0600

    customize weather fetch api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,10 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=3914ebf557762e19043de057a45be473&query=New York';
+const url = 'http://api.weatherstack.com/current?access_key=3914ebf557762e19043de057a45be473&query=Bangladesh&units=f';
 
-request({ url: url} , (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json:true } , (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ' .It is currently ' + response.body.current.temperature + ' degress out. There is a '
+        + response.body.current.feelslike + '% chance of rain' );
 })
 
 
