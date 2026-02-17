3701853fea82c85faa9baf61141ec0ad905818aa
Dip Ghosh <dipnsucse@gmail.com>
2026-02-17 21:49:34 +0600

    send back json html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,29 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express World!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help Page!');
+    res.send([
+        {
+        name: 'Dip'
+        },
+        {
+            age: 30
+        }
+        ]);
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page!');
+    res.send('<h1>About Page! </h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page!');
+    res.send({
+        forecast: 'Weather Weather',
+        location: 'Dhaka'
+    });
 })
 
 app.listen(3000, () => {
