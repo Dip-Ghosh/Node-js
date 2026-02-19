142fa68224760a96e3fddca0ac8dd22b68129a45
Dip Ghosh <dipnsucse@gmail.com>
2026-02-19 12:20:41 +0600

    templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,44 @@
+
 const path = require("path");
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+//define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Dip'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Dip'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is the help page',
+        title: 'Help',
+        name: 'Dip'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
