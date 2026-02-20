ec21d6d21e6ef5c0fd0331226f5a0d3f6111a3f1
Dip Ghosh <dipnsucse@gmail.com>
2026-02-20 22:57:45 +0600

    weather app done

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,24 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const message1 = document.querySelector("#message-1");
+const message2 = document.querySelector("#message-2");
 
-fetch("http://localhost:3000/weather?address=Dhaka").then((response) =>
-    response.json()).then((data) => {
-        if (data.error) {
-            alert(data.error);
-        } else {
-            console.log(data)
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    message1.textContent = 'Loading...';
+    message2.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>
+        response.json()).then((data) => {
+            if (data.error) {
+                message1.textContent = data.error;
+            } else {
+                message2.textContent = data.location + data.address
+                message1.textContent = data.forecast.name + " " + data.forecast.country + " " + data.forecast.temperature + " " + data.forecast.feelslike;
+            }
         }
-    }
-);
+    );
+
+})
