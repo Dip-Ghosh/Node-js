16b1540d6638cebf316e382d8cc179967c128401
Dip Ghosh <dipnsucse@gmail.com>
2026-03-13 00:21:29 +0600

    playground js async and await

--- task-manager/playground/playground.js
+++ task-manager/playground/playground.js
@@ -20,3 +20,25 @@ Task.findByIdAndDelete('69aefb133c3f145697af5fed').then((task)=> {
 }).catch((err)=> {
     console.log(err);
 })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age});
+    return await User.countDocuments({age});
+}
+
+
+updateAgeAndCount('69aef8705295b1338a5ba070', 2).then((count) => {
+    console.log(count);
+}).catch((err) => {
+    console.log(err);
+})
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    return await Task.countDocuments({completed: false});
+}
+
+deleteTaskAndCount('69aefb608003f147118d97be').then((count) => {
+    console.log(count)
+}).catch((err) => {console.log(err)})
