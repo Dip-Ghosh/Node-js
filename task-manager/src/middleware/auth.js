7a0c26c29324d25f8cdd82b5c02b8972d40555dc
Dip Ghosh <dipnsucse@gmail.com>
2026-03-20 02:56:49 +0600

    logout and logout all done

---
+++ task-manager/src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+
+        if (!token) {
+            throw new Error('Token is missing');
+        }
+
+        const decodedToken = jwt.verify(token, 'secret');
+        const user = await User.findById({_id:decodedToken._id, 'tokens.token':token})
+
+        if (!user) {
+            throw new Error('UNAUTHORIZED');
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+
+    } catch (error) {
+        res.status(401).send({error: 'Please authenticate'});
+    }
+}
+
+module.exports = auth;
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
-router.post('/users', async (req, res, next) => {
+router.post('/users', async (req, res) => {
     const user = new User(req.body);
 
     try {
@@ -19,22 +20,52 @@ router.post('/users', async (req, res, next) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token =  await user.generateAuthToken()
+        const token = await user.generateAuthToken()
 
         res.status(200).send({user, token});
-    }catch (error) {
+    } catch (error) {
         console.log(error);
-        res.status(400).send( error);
+        res.status(400).send(error);
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const users = await User.find();
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+
+        res.status(200).send('logout')
+    }catch(error) {
+        res.status(500).send(error);
+    }
+})
+
+//logout all session
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+
+        req.user.tokens = [];
+        await req.user.save();
+        res.status(200).send('logout')
+    } catch (er) {
+        res.status(500).send(er);
+    }
+})
+
+router.get('/users',auth, async (req, res) => {
+    try {
+        const users = await User.find({_id:req});
         res.status(200).send(users);
     } catch (error) {
         res.status(500).send(error);
     }
+})
+
+router.get('/users/me',auth, async (req, res) => {
+   res.status(200).send({user: req.user});
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -65,7 +96,7 @@ router.patch('/users/:id', async (req, res, next) => {
         const user = await User.findById(req.params.id);
 
         if (!user) {
-            return res.status(404).send({ error: 'User not found' });
+            return res.status(404).send({error: 'User not found'});
         }
 
         updates.forEach((update) => user[update] = req.body[update])
